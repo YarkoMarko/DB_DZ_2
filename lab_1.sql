ad0bed7f3a9df8fe745897570120860fc9165741
YarkoMarko <Yarema1310@gmail.com>
2024-01-21 22:41:08 +0200

    Table Filling

--- lab_1.sql
+++ lab_1.sql
@@ -1,7 +1,23 @@
-CREATE TABLE Stuff(
-	element_id SERIAL PRIMARY KEY,
-	nameOF VARCHAR(50),
-	typeOF VARCHAR(50),
-	colour VARCHAR(50),
-	caloricContent INT,
-	description TEXT)
+-- CREATE TABLE Stuff(
+-- 	element_id SERIAL PRIMARY KEY,
+-- 	nameOF VARCHAR(50),
+-- 	typeOF VARCHAR(50),
+-- 	colour VARCHAR(50),
+-- 	caloricContent INT,
+-- 	description TEXT)
+
+-- INSERT INTO Stuff(
+-- 	nameOF,
+-- 	typeOF,
+-- 	colour,
+-- 	caloricContent,
+-- 	description)
+-- VALUES
+--   ('Carrot', 'Vegetable', 'Orange', 41, 'A healthy and crunchy vegetable.'),
+--   ('Apple', 'Fruit', 'Red', 95, 'Sweet and juicy fruit.'),
+--   ('Banana', 'Fruit', 'Yellow', 105, 'A tropical and energy-packed fruit.'),
+--   ('Broccoli', 'Vegetable', 'Green', 55, 'Nutrient-rich green vegetable.'),
+--   ('Grapes', 'Fruit', 'Purple', 69, 'Small and delicious grapes.'),
+--   ('Spinach', 'Vegetable', 'Green', 23, 'Leafy green vegetable with various health benefits.');
+
+SELECT * FROM Stuff
